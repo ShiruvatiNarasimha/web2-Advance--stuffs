d37efe2bd0a35757c974aef035dba799dd33845e
Shiruvati <mrnattyboy05@gmail.com>
2025-05-29 15:14:07 +0530

    Update index.js

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -3,7 +3,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
 const PORT = 3000;
 // Store OTPs in a simple in-memory object
 // Endpoint to generate and log OTP
